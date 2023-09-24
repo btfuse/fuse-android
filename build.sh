14c69af1fca31624cf40abd816fe6a66dfed8bfd
Norman Breau <norman@nbsolutions.ca>
2023-09-24 19:25:15 -0300

    update build scripts

--- build.sh
+++ build.sh
@@ -14,35 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source ../build-tools/assertions.sh
-
-assertMac "Mac is required for publishing"
-assertGitRepo
-assertCleanRepo
-
-VERSION="$1"
-
-assertVersion $VERSION
-
-assetGitTagAvailable $VERSION
-
-./gradlew :fuse:test
-assertLastCall
-
-echo $VERSION > VERSION
-
 ./gradlew :fuse:build
-assertLastCall
-
-git add VERSION
-git commit -m "Android Release: $VERSION"
-git push
-git tag -a $VERSION -m "Android Release: $VERSION"
-git push --tags
-
-./gradlew publishReleasePublicationToMavenRepository
-
-gh release create $VERSION \
-    ./fuse/build/outputs/aar/fuse-debug.aar \
-    ./fuse/build/outputs/aar/fuse-release.aar \
-    --verify-tag --generate-notes
