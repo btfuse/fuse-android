932b46b58539fd7f2d2e7e35d4b51fab867cf56b
Norman Breau <norman@nbsolutions.ca>
2023-10-21 20:38:56 -0300

    nbsolutions -> breautek namespace

--- fuse/src/main/java/ca/nbsolutions/fuse/plugins/IFusePluginRegistrar.java
+++
@@ -1,7 +0,0 @@
-package ca.nbsolutions.fuse.plugins;
-
-import ca.nbsolutions.fuse.FusePlugin;
-
-public interface IFusePluginRegistrar {
-    void registerPlugin(FusePlugin plugin);
-}
---
+++ fuse/src/main/java/com/breautek/fuse/FuseContext.java
@@ -0,0 +1,310 @@
+
+/*
+Copyright 2023 Norman Breau
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.breautek.fuse;
+
+import androidx.annotation.NonNull;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewAssetLoader;
+import androidx.webkit.WebViewClientCompat;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.breautek.fuse.plugins.FuseRuntime;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class FuseContext {
+    private static final String TAG = "FuseContext";
+
+    private final AppCompatActivity $context;
+
+    private WebView $webview;
+
+    /*package private*/ final ReadWriteLock $pluginMapLock;
+    private final Map<String, FusePlugin> $pluginMap;
+
+    private final FuseAPIRouter $apiRouter;
+
+    public static final String SCHEME = "https";
+    public static final String HOST = "localhost";
+
+    private final Handler $mainThread;
+
+    private final FuseAPIServer $apiServer;
+
+    private final PermissionRequestHandler $permissionRequestHandler;
+
+    private FuseAPIResponseFactory $responseFactory;
+
+    private FuseLogger $logger;
+
+    public FuseContext(AppCompatActivity context) {
+        $context = context;
+        $logger = new FuseLogger(this);
+        $responseFactory = new FuseAPIResponseFactory();
+        $mainThread = new Handler(Looper.getMainLooper());
+        $permissionRequestHandler = _createPermissionRequest();
+        $pluginMapLock = new ReentrantReadWriteLock();
+        $pluginMap = new HashMap<String, FusePlugin>();
+        $apiRouter = new FuseAPIRouter(this);
+
+        try {
+            $apiServer = new FuseAPIServer(this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Log.i(TAG, "API Server Port: " + $apiServer.getPort());
+
+        registerPlugin(new FuseRuntime(this));
+    }
+
+    public void setResponseFactory(FuseAPIResponseFactory factory) {
+        $responseFactory = factory;
+    }
+
+    public FuseAPIResponseFactory getResponseFactory() {
+        return $responseFactory;
+    }
+
+    protected PermissionRequestHandler _createPermissionRequest() {
+        return new PermissionRequestHandler(this);
+    }
+
+    public FuseLogger getLogger() {
+        return $logger;
+    }
+
+    public PermissionRequestHandler getPermissionRequestHandler() {
+        return $permissionRequestHandler;
+    }
+
+    public boolean isDebug() {
+        try {
+            Context context = getActivityContext();
+            PackageManager pm = context.getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(context.getPackageName(), 0);
+            return (appInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (PackageManager.NameNotFoundException e) {
+            // Handle the exception as needed.
+            return false; // Return a default value in case of an error.
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public void onCreate(Bundle bundle) {
+        if (isDebug()) {
+            int logLevel = $logger.getLevel();
+            logLevel |= FuseLoggerLevel.DEBUG;
+            $logger.setLevel(logLevel);
+        }
+
+        $logger.info(TAG, "Fuse Version: " + BuildConfig.FUSE_VERSION);
+
+        $webview = new WebView($context);
+
+        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler($context))
+                .setHttpAllowed(false)
+                .setDomain(HOST)
+                .build();
+
+        $webview.setWebViewClient(new WebViewClientCompat() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            $logger.info(TAG, "DOM Request: " + request.getUrl());
+            return assetLoader.shouldInterceptRequest(request.getUrl());
+            }
+        });
+
+        WebSettings settings = $webview.getSettings();
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        $webview.setWebChromeClient(new WebChromeClient());
+        $webview.addJavascriptInterface(this, "NBSNative");
+        $webview.loadUrl("https://localhost/assets/index.html");
+    }
+
+    public void onLowMemory() {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onLowMemory();
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public void onPause() {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onPause();
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public void onResume() {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onResume();
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onSaveInstanceState(outState);
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public void onStart() {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onStart();
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onActivityResult(requestCode, resultCode, data);
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public void onStop() {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onStop();
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        $permissionRequestHandler.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void onDestroy() {
+        $pluginMapLock.readLock().lock();
+        for (FusePlugin plugin : $pluginMap.values()) {
+            plugin.onDestroy();
+        }
+        $pluginMapLock.readLock().unlock();
+    }
+
+    public WebView getWebview() {
+        return $webview;
+    }
+
+    public AppCompatActivity getActivityContext() {
+        return $context;
+    }
+
+    public void registerPlugin(FusePlugin plugin) {
+        $pluginMapLock.writeLock().lock();
+        $registerPlugin(plugin);
+        $pluginMapLock.writeLock().unlock();
+    }
+
+    /*package private*/ void $registerPlugin(FusePlugin plugin) {
+        if ($pluginMap.containsKey(plugin.getID())) {
+            $logger.warn(TAG, "Plugin \"" + plugin.getID() + "\" is already registered.");
+            return;
+        }
+
+        $pluginMap.put(plugin.getID(), plugin);
+    }
+
+    public FusePlugin getPlugin(String pluginID) {
+        return $pluginMap.getOrDefault(pluginID, null);
+    }
+
+    public FuseAPIRouter getAPIRouter() {
+        return $apiRouter;
+    }
+
+    @JavascriptInterface
+    public String getAPISecret() {
+        return $apiServer.getSecretKey();
+    }
+
+    @JavascriptInterface
+    public int getAPIPort() {
+        return $apiServer.getPort();
+    }
+
+    @JavascriptInterface
+    public void setLogCallback(String callbackID) {
+        $logger.setCallbackID(callbackID);
+    }
+
+    @JavascriptInterface
+    public void log(int level, String message) {
+        String tag = "FuseWebviewRuntime";
+
+        switch (level) {
+            case FuseLoggerLevel.DEBUG:
+                Log.d(tag, message);
+                break;
+            case FuseLoggerLevel.INFO:
+                Log.i(tag, message);
+                break;
+            case FuseLoggerLevel.WARN:
+                Log.w(tag, message);
+                break;
+            case FuseLoggerLevel.ERROR:
+                Log.e(tag, message);
+                break;
+        }
+    }
+
+    public void execCallback(String callbackID, String payload) {
+        $mainThread.post(() -> {
+            $webview.evaluateJavascript(String.format("window.__nbsfuse_doCallback(\"%s\",\"%s\");", callbackID, payload.replace("\"", "\\\"")), null);
+        });
+    }
+
+    public void execCallback(String callbackID) {
+        $mainThread.post(() -> {
+            $webview.evaluateJavascript(String.format("window.__nbsfuse_doCallback(\"%s\");", callbackID), null);
+        });
+    }
+
+    public void runOnMainThread(Runnable runnable) {
+        $mainThread.post(runnable);
+    }
+}
